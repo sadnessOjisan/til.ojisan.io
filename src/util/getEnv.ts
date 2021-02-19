6e6da6aebde284315797e5d9ea2314007c31f2d1
sadness_ojisan <sadness.ojisan@gmail.com>
2021-02-20 03:39:47 +0900

    invalid env

--- src/util/getEnv.ts
+++ src/util/getEnv.ts
@@ -1,8 +1,13 @@
-import { ENVS } from "../const/env";
+import { ENVS, ENV_TYPE } from "../const/env";
 
 export const getNodeEnv = () => {
-  const env = process.env.NODE_ENV;
+  const env = process.env.NEXT_PUBLIC_BUILD_ENV;
   if (env === "test") throw new Error("invalid env");
-  if (!ENVS.includes(env)) throw new Error("invalid env");
+  if (!isValidEnv(env)) throw new Error("invalid env");
   return env;
+};
+
+export const isValidEnv = (env: any): env is ENV_TYPE => {
+  if (!ENVS.includes(env)) return false;
+  return true;
 };
