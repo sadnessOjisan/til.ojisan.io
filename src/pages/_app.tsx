09c3ee5838afb2203088bae70dc35440b4eb4206
sadness_ojisan <sadness.ojisan@gmail.com>
2021-02-22 03:39:28 +0900

    adjust color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import "../style/markdown-body.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="text-white bg-black min-h-screen text-lg p-8">
+    <div className="min-h-screen text-lg p-8 text-gray-300 bg-gray-800">
       <div className="max-w-screen-lg mx-auto">
         <Component {...pageProps} />
       </div>
