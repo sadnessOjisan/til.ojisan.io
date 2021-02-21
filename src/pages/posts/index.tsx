6945e26bf9fd6553b9c3242a6f17fd05ce07b6b4
sadness_ojisan <sadness.ojisan@gmail.com>
2021-02-22 02:44:41 +0900

    border color

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,22 +3,30 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { toPostFromResponse } from "../../../types/model";
 import { isValidPostsResponse, PostsResponse } from "../../../types/response";
+import { format } from "../../util/date";
 import { getHost } from "../../util/getHost";
 
 export default (postsResponse: { data: PostsResponse }) => {
   const posts = postsResponse.data.map((res) => toPostFromResponse(res));
   return (
     <div>
-      <h1 className="text-center">Today ojIsan Learned</h1>
+      <h1 className="text-center text-3xl">
+        <span className="text-blue-700">T</span>oday oj
+        <span className="text-blue-700">I</span>san
+        <span className="text-blue-700">L</span>earned
+      </h1>
       {posts.map((post) => (
         <Link href={`posts/${post.id}`}>
           <a>
-            <div>
-              <h2>{post.title}</h2>
-              <div>
-                {post.tags.map((tag) => (
-                  <span>{tag}</span>
-                ))}
+            <div className="flex flex-row my-4 items-center border-white border-solid border-2 p-2 rounded-md hover:border-blue-700">
+              <div className="w-28">{format(post.timeStamp)}</div>
+              <div className="flex flex-col">
+                <h2 className="mr-4">{post.title}</h2>
+                <div>
+                  {post.tags.map((tag) => (
+                    <span className="mr-4">{tag}</span>
+                  ))}
+                </div>
               </div>
             </div>
           </a>
