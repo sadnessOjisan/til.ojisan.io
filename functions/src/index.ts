cd587ea7f2c929fc19cc6236d08371cba900e033
sadness_ojisan <sadness.ojisan@gmail.com>
2021-02-23 04:41:44 +0900

    delete

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -459,3 +459,60 @@ const _isValidEditRequestBody = (body: any): body is EditRequest => {
   }
   return true;
 };
+
+export const deletePostById = functions
+  .region("asia-northeast1") // TODO: 関数の先頭は共通化できそう
+  .https.onRequest((request, response) => {
+    response.set("Access-Control-Allow-Origin", "*");
+    response.set(
+      "Access-Control-Allow-Methods",
+      "GET, HEAD, OPTIONS, POST, DELETE"
+    );
+    response.set("Access-Control-Allow-Headers", "Content-Type, authorization");
+    if (request.method !== "POST") {
+      response
+        .status(400)
+        .json({ error: `${request.method} is invalid method` });
+      return;
+    }
+    try {
+      JSON.parse(request.body);
+    } catch (e) {
+      response.status(400).json({ error: `${request.body} cannot parse` });
+      throw new Error("body parse error");
+    }
+    const parsedBody = JSON.parse(request.body);
+    if (!_isValidDeleteRequestBody(parsedBody)) {
+      console.error(`${JSON.stringify(request.body)} is invalid request`);
+      response.status(400).json({ error: "invalid request" });
+      return;
+    }
+
+    // post の保存
+    try {
+      db.collection(COLLECTION_KEY.POSTS)
+        .doc(parsedBody.id)
+        .delete()
+        .then(() => {
+          response.status(200).json("success");
+        })
+        .catch(() => {
+          console.error("fail to save edit data");
+        });
+    } catch (e) {
+      console.error(e);
+      response.status(500).json({ error: "fail to save post" });
+    }
+  });
+
+const _isValidDeleteRequestBody = (body: any): body is { id: string } => {
+  if (body === undefined || body === null) {
+    console.error("body should be there");
+    return false;
+  }
+  if (typeof body.id !== "string") {
+    console.error("id should be there");
+    return false;
+  }
+  return true;
+};
