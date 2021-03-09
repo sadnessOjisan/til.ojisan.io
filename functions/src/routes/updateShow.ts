d75d22d91fa9e483ea949d8a561c12ec3711d456
sadness_ojisan <sadness.ojisan@gmail.com>
2021-03-09 16:55:05 +0900

    fix

--- functions/src/routes/updateShow.ts
+++ functions/src/routes/updateShow.ts
@@ -2,21 +2,30 @@ import * as functions from "firebase-functions";
 import { updateShow } from "../service/post/updateShowFlg";
 import { isValidUpdateShowRequest } from "../types/request/update-show-request";
 
-//   tilを一つ取得
+// update
 export const updateShowFlg = functions
   .region("asia-northeast1")
   .https.onRequest(async (request, response) => {
-    response.set("Access-Control-Allow-Origin", "*");
-    response.set(
-      "Access-Control-Allow-Methods",
-      "GET, HEAD, OPTIONS, POST, DELETE"
-    );
-    response.set("Access-Control-Allow-Headers", "Content-Type, authorization");
-    const body = request.body;
-    if (!isValidUpdateShowRequest(body)) {
-      response.status(400).json({ error: "invalid requestrequest" });
-      throw new Error("invalid requestrequest");
+    if (request.method === "OPTIONS") {
+      response.status(204).send("");
+    } else if (request.method === "POST") {
+      response.set("Access-Control-Allow-Origin", "*");
+      response.set(
+        "Access-Control-Allow-Methods",
+        "GET, HEAD, OPTIONS, POST, DELETE"
+      );
+      response.set(
+        "Access-Control-Allow-Headers",
+        "Content-Type, authorization"
+      );
+      const body = request.body;
+      if (!isValidUpdateShowRequest(body)) {
+        response.status(400).json({ error: "invalid requestrequest" });
+        throw new Error("invalid requestrequest");
+      }
+      await updateShow(body.post_id, body.show);
+      response.status(204).json("");
+    } else {
+      response.status(403).json("no method");
     }
-    await updateShow(body.post_id, body.show);
-    response.status(204).json("");
   });
