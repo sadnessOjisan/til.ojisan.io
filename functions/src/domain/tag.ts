77d4d48fa5bb6065373a622020495c9de0fa36fb
sadness_ojisan <sadness.ojisan@gmail.com>
2021-04-10 20:04:32 +0900

    tag:wq

---
+++ functions/src/domain/tag.ts
@@ -0,0 +1,35 @@
+export class Tag {
+  private _name: string;
+  private _timeStamp: Date; // TODO: ISO型を作る
+  private _id: string;
+
+  private constructor(arg: {
+    name: string;
+    timeStamp: Date; // TODO: ISO型を作る
+    id: string;
+  }) {
+    this._name = arg.name;
+    this._timeStamp = arg.timeStamp;
+    this._id = arg.id;
+  }
+
+  static createTag = (arg: {
+    name: string;
+    timeStamp: Date; // TODO: ISO型を作る
+    id: string;
+  }) => {
+    return new Tag(arg);
+  };
+
+  get name() {
+    return this._name;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get timeStamp() {
+    return this._timeStamp;
+  }
+}
